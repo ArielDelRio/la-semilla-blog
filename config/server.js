78ef4fe04a99558b599393a3653e5a40956c534a
ArieldRio <arieldelrioviamonte@gmail.com>
2021-08-08 20:49:18 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c096b7566ef3706ee5bcdd65c6afb8ec'),
+    },
+  },
+});
